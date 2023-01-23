1a0e6b5db3f122b0e0f83de97b25068c8eb9b760
dhikaaaa <dhikabahri4@gmail.com>
2023-01-23 20:14:07 +0700

    main.skills done

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,22 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+// Accordion Skills
+const skillsContent = document.getElementsByClassName('skills-content'),
+  skillsHeader = document.querySelectorAll('.skills-header');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills-content skills-close';
+  }
+
+  if (itemClass === 'skills-content skills-close') {
+    this.parentNode.className = 'skills-content skills-open';
+  }
+}
+skillsHeader.forEach((element) => {
+  element.addEventListener('click', toggleSkills);
+});
