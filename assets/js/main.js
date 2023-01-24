854d07c9b0bb1bbc623f269e67e6d6e1875250f9
dhikaaaa <dhikabahri4@gmail.com>
2023-01-24 13:21:16 +0700

    main.qualification done

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,23 @@ function toggleSkills() {
 skillsHeader.forEach((element) => {
   element.addEventListener('click', toggleSkills);
 });
+
+// Qualification Tabs
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification-active');
+    });
+    target.classList.add('qualification-active');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification-active');
+    });
+    tab.classList.add('qualification-active');
+  });
+});
